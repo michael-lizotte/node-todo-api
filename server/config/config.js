f6e601e77e0af70c0658bb4946a6686cc3450c35
michael-lizotte <michael.lizotte-gagnon1@uqac.ca>
2018-10-26 14:16:09 -0400

    Setup app for heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,7 @@
 var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoApp'
+    process.env.MONGODB_URI = 'mongodb://admin:banane123@ds047592.mlab.com:47592/node-todo-api'
 } else if (env === 'test') {
-    process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoAppTest'
 }
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-var port = process.env.PORT;
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
