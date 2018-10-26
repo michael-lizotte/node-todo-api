d2b5ef8f43202402d113ee8af243d28d6c734be7
michael-lizotte <michael.lizotte-gagnon1@uqac.ca>
2018-10-26 15:52:29 -0400

    Implemented password hashing wih bcryptjs

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA512} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-var decoded = jwt.verify(token, '123abc')
-console.log(decoded);
+var hashedPassword = '$2a$10$bmN7QY1bn5EkzTZCYyxEruuJlJegxpQnIwmgb8remHAZZ6kDiST.W'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc')
+// console.log(decoded);
 
 // var data = {
 //     id: 4
