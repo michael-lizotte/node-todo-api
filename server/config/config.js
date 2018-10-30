219208245d8fceb3cf77c0dd495f15c5eb656547
michael-lizotte <michael.lizotte-gagnon1@uqac.ca>
2018-10-30 14:29:26 -0400

    Small changes to config file

--- server/config/config.js
+++ server/config/config.js
@@ -3,10 +3,10 @@ var mongoDbUrl = '';
 
 console.log(env);
 
-if(env === 'development') {
-    mongoDbUrl = 'mongodb://admin:banane123@ds047592.mlab.com:47592/node-todo-api'
-} else if (env === 'test') {
+if(env === 'test') {
     mongoDbUrl = 'mongodb://test:test123@ds145573.mlab.com:45573/node-todo-api-test'
+} else {
+    mongoDbUrl = 'mongodb://admin:banane123@ds047592.mlab.com:47592/node-todo-api'    
 }
 
 module.exports = {
